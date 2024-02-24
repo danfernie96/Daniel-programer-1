87d4cacc23ebb459e5be0cc1f2f2ad9d1cb522aa
danfernie96 <161092918+danfernie96@users.noreply.github.com>
2024-02-23 21:19:25 -0500

    Add files via upload

---
+++ pythonProject3/Viernes Daniel F.py
@@ -0,0 +1,12 @@
+numero1 = 10
+numero2 = 20
+
+suma = numero1 + numero2
+resta = numero1 - numero2
+multiplicacion = numero1 * numero2
+division = numero1 / numero2
+
+print (suma)
+print (resta)
+print (multiplicacion)
+print (division)
